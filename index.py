d4b9508f81ea6aba94d08b44cebfc4938d31a10d
Roberto <oliveirarobertw@gmail.com>
2025-01-31 15:14:49 -0300

    adding coments

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
+# Carregar dados
 df_clientes = pd.read_csv('data\clientes.csv', sep=';', decimal='.')
 df_clientes['Aniversario'] = pd.to_datetime(df_clientes['Aniversario'], dayfirst=True)
 df_clientes
@@ -14,3 +15,5 @@ df_entradas
 df_saidas = pd.read_csv('data\saidas.csv', sep=';', decimal='.')
 df_saidas['Data'] = pd.to_datetime(df_saidas['Data'], dayfirst=True)
 df_saidas
+
+# Calcular os totais
