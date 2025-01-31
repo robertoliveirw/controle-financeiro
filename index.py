6f7645b76d274edb0111b8feabf3354ea430db85
Roberto <oliveirarobertw@gmail.com>
2025-01-31 14:59:22 -0300

    adding dayfisrt

--- index.py
+++ index.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
-df_clientes = pd.read_csv('data\clientes.csv', sep=';', decimal='.')
+df_clientes = pd.read_csv('data\clientes.csv', sep=';', decimal='.', dayfirst=True)
 df_clientes
 
-df_entradas = pd.read_csv('data\entradas.csv', sep=';', decimal='.')
+df_entradas = pd.read_csv('data\entradas.csv', sep=';', decimal='.', dayfirst=True)
 df_entradas
 
-df_saidas = pd.read_csv('data\saidas.csv', sep=';', decimal='.')
+df_saidas = pd.read_csv('data\saidas.csv', sep=';', decimal='.', dayfirst=True)
 df_saidas
