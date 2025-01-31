951cfc2b83fce35492f725ce4eb1e8a70efa104e
Roberto <oliveirarobertw@gmail.com>
2025-01-31 15:06:33 -0300

    fixing bugs

--- index.py
+++ index.py
@@ -3,11 +3,14 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
-df_clientes = pd.read_csv('data\clientes.csv', sep=';', decimal='.', dayfirst=True)
+df_clientes = pd.read_csv('data\clientes.csv', sep=';', decimal='.')
+df_clientes['Aniversario'] = pd.to_datetime(df_clientes['Aniversario'], dayfirst=True)
 df_clientes
 
-df_entradas = pd.read_csv('data\entradas.csv', sep=';', decimal='.', dayfirst=True)
+df_entradas = pd.read_csv('data\entradas.csv', sep=';', decimal='.')
+df_entradas['Data da Entrada'] = pd.to_datetime(df_entradas['Data da Entrada'], dayfirst=True)
 df_entradas
 
-df_saidas = pd.read_csv('data\saidas.csv', sep=';', decimal='.', dayfirst=True)
+df_saidas = pd.read_csv('data\saidas.csv', sep=';', decimal='.')
+df_saidas['Data'] = pd.to_datetime(df_saidas['Data'], dayfirst=True)
 df_saidas
